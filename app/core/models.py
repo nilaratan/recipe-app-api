293d9475e893e70a224cbcab190fd1f931635129
tanmaya <nayaknayak111@gmail.com>
2022-12-16 00:26:21 +0530

    recipe added

--- app/core/models.py
+++ app/core/models.py
@@ -44,7 +44,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Recipe(models.Model):
     """Recipe object"""
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
