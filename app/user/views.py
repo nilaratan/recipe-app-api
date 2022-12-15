22b72ffbc5396860e9d2cd3a5e3bd0c04ce284bd
tanmaya <nayaknayak111@gmail.com>
2022-12-16 00:29:17 +0530

    recipe added

--- app/user/views.py
+++ app/user/views.py
@@ -5,7 +5,6 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from . import serializers
-from django.contrib.auth import get_user_model
 
 
 class CreateUserView(generics.CreateAPIView):
